47f980637e70a5cb122b3f02d5fb85f91250bdd5
Nicole Dow <nikki.dow71@gmail.com>
2019-05-23 18:45:25 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -27,7 +27,14 @@ def run_guessing_game
 puts "Guess a number between 1 and 6."
 
 user_input = gets.chomp
-computer_guess = 
+computer_guess = rand(1..6)
+
+while user_input != "exit" do
+  if user_input.to_i == computer_guess
+    puts "You guessed correctly!"
+  
+
+end
 
 end
 
